de9ebd041b5191048f289e4f83ef5fe5968cccdb
sonaliprasadika <sonaliprasadika077@gmail.com>
2023-11-05 19:38:21 +0200

    add crud operations

---
+++ config.js
@@ -0,0 +1,43 @@
+'use strict';
+const dotenv = require('dotenv');
+const assert = require('assert');
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./keys/seizon-app-firebase-adminsdk-91yl2-6753231b2e.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+dotenv.config();
+
+const {
+    PORT,
+    HOST,
+    HOST_URL,
+    API_KEY,
+    AUTH_DOMAIN,
+    DATABASE_URL,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID
+} = process.env;
+
+assert(PORT, 'PORT is required');
+assert(HOST, 'HOST is required');
+
+module.exports = {
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    firebaseConfig: {
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        databaseURL: DATABASE_URL,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID
+    }
+}
