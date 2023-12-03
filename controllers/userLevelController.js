5705a310ebaab6d7d1e3827a7a59c8a8bd7afae6
sonaliprasadika <sonaliprasadika077@gmail.com>
2023-12-04 00:09:38 +0200

    add user_level controller

--- controllers/userLevelController.js
+++ controllers/userLevelController.js
@@ -13,85 +13,20 @@ const addUserLevel = async (req, res, next) => {
             userLevelData.collected_points
             );
         const userLevelRef = await db.collection('UserLevel').add(JSON.parse(JSON.stringify(userLevel)));
-        console.log(JSON.parse(JSON.stringify(goal)))
+        console.log(JSON.parse(JSON.stringify(userLevel)))
         res.send(`User record saved successfully with ID: ${userLevelRef.id}`);
     } catch (error) {
         res.status(400).send(error.message);
     }
 }
 
-const getAllGoals = async (req, res, next) => {
+const getLevelbyUser = async (req, res, next) => {
     try {
-        const users = await db.collection('Goals');
-        const data = await users.get();
-        const usersArray = [];
-
-        if (data.empty) {
-            res.status(404).send('No user records found');
-        } else {
-            data.forEach(doc => {
-                const userData = {};
-                const docData = doc.data();
-                
-                for (const key in docData) {
-                    if (docData.hasOwnProperty(key)) {
-                        userData[key] = docData[key];
-                    }
-                }
-
-                usersArray.push(userData);
-            });
-
-            res.send(usersArray);
-        }
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-}
-
-const getAllGoalsByUser = async (req, res, next) => {
-    try {
-        const userID = req.params.id;
-        const userGoals= await db.collection('Goals').where('user_id', '==', userID).get();
-
-        console.log('Number of Documents:', userGoals.docs.length);
-        console.log('Document Data:', userGoals.docs.map(doc => doc.data()));
-
-        if (userGoals.empty) {
-            console.log(`No user record found with id: ${userID}`);
-            return res.status(404).send('No user records found');
-        }
-        const usersArray = [];
-
-        if (userGoals.empty) {
-            res.status(404).send('No user records found');
-        } else {
-            userGoals.forEach(doc => {
-                const userData = {};
-                const docData = doc.data();
-                
-                for (const key in docData) {
-                    if (docData.hasOwnProperty(key)) {
-                        userData[key] = docData[key];
-                    }
-                }
-
-                usersArray.push(userData);
-            });
-
-            res.send(usersArray);
-        }
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-};
-
-const getGoalbyUser = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const g_id = req.params.g_id;
-        
-        const querySnapshot = await db.collection('Goals').where('user_id', '==', id).get(g_id);
+        const id = req.user.id;
+        const g_id = req.params.id;
+        console.log(id)
+        console.log(g_id)
+        const querySnapshot = await db.collection('UserLevel').where('user_id', '==', id).get(g_id);
 
         if (querySnapshot.empty) {
             res.status(404).send('User with the given ID and Goal ID not found');
@@ -105,11 +40,11 @@ const getGoalbyUser = async (req, res, next) => {
     }
 };
 
-const updateGoal = async (req, res, next) => {
+const updateUserLevel = async (req, res, next) => {
     try {
         const id = req.params.id;
         const data = req.body;
-        const user =  await db.collection('Goals').doc(id);
+        const user =  await db.collection('UserLevel').doc(id);
         await user.update(data);
         res.send('User record updated successfuly');        
     } catch (error) {
@@ -117,10 +52,10 @@ const updateGoal = async (req, res, next) => {
     }
 }
 
-const deleteGoal = async (req, res, next) => {
+const deleteUserLevel = async (req, res, next) => {
     try {
         const id = req.params.id;
-        await db.collection('Goals').doc(id).delete();
+        await db.collection('UserLevel').doc(id).delete();
         res.send('Record deleted successfuly');
     } catch (error) {
         res.status(400).send(error.message);
@@ -129,9 +64,7 @@ const deleteGoal = async (req, res, next) => {
 
 module.exports = {
     addUserLevel,
-    getAllGoals,
-    getAllGoalsByUser,
-    getGoalbyUser,
-    updateGoal,
-    deleteGoal
+    getLevelbyUser,
+    updateUserLevel,
+    deleteUserLevel
 }
