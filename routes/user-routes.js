86937e700ac9fc497eec46d78e0cdd819624eea0
sonaliprasadika <sonaliprasadika077@gmail.com>
2023-12-27 10:48:34 +0200

    change the port

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // const { authenticate } = require('../controllers/userController');
-const {addUser,
+const {
         getAllUsers,
         getUser,
         updateUser,
@@ -14,61 +14,113 @@ const router = express.Router();
  * @swagger
  * components:
  *   schemas:
- *     Book:
+ *     User:
  *       type: object
  *       required:
- *         - title
- *         - author
- *         - finished
+ *         - username
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - password
  *       properties:
- *         id:
+ *         username:
  *           type: string
- *           description: The auto-generated id of the book
- *         title:
+ *         firstName:
  *           type: string
- *           description: The title of your book
- *         author:
- *           type: string
- *           description: The book author
- *         finished:
+ *         lastName:
  *           type: boolean
- *           description: Whether you have finished reading the book
- *         createdAt:
+ *         email:
  *           type: string
- *           format: date
- *           description: The date the book was added
+ *         user_language:
+ *           type: string
+ *         height:
+ *           type: float
+ *         weight:
+ *           type: float
+ *         age:
+ *           type: integer
+ *         gender:
+ *           type: string
+ *         password:
+ *           type: string
+ *           description: The password is hashed by server
+ *         user_level:
+ *           type: integer
+ *           description: The current level of the user
+ *         total_steps:
+ *           type: integer
+ *           description: Total steps user has achived
  *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- *         finished: false
- *         createdAt: 2020-03-10T04:05:06.157Z
+ *         username: john_doe
+ *         firstName: John
+ *         lastName: Doe
+ *         email: john.doe@example.com
+ *         user_language: en_US
+ *         height: 180
+ *         weight: 75
+ *         age: 30
+ *         gender: male
+ *         password: secure_password123
+ *         user_level: 3
+ *         total_steps: 5000
  */
 
 /**
  * @swagger
  * tags:
- *   name: Books
- *   description: The books managing API
- * /books:
+ *   name: User
+ *   description: The User managing API
+ * /api/user/register:
  *   post:
- *     summary: Create a new book
- *     tags: [Books]
+ *     summary: Register a new user
+ *     tags: [User]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/Book'
+ *             $ref: '#/components/schemas/User'
  *     responses:
  *       200:
- *         description: The created book.
+ *         description: The user has been registered.
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/Book'
+ *               $ref: '#/components/schemas/User'
  *       500:
- *         description: Some server error
+ *         description: Server error
+ * /api/user/login:
+ *   post:
+ *     summary: Login existing user
+ *     tags: [User]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The user has been registered.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Server error
+ * /api/users:
+ *   get:
+ *     summary: List of all users
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: List of all users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                $ref: '#/components/schemas/User'
  *
  */
 router.post('/user/register', userRegister);
