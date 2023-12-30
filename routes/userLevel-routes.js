36673dc5018128fcc8eb59c9c7d05236aae4a253
sonaliprasadika <sonaliprasadika077@gmail.com>
2023-12-30 13:38:25 +0200

    swagger updates

--- routes/userLevel-routes.js
+++ routes/userLevel-routes.js
@@ -8,6 +8,121 @@ const {
 
 const router = express.Router();
 
+//UserLevel Schema
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *   schemas:
+ *     UserLevel:
+ *       type: object
+ *       required:
+ *         - level_id
+ *         - collected_points
+ *       properties:
+ *         level_id:
+ *           type: string
+ *         collected_points:
+ *           type: string
+ *           description: collected points by user
+ *       example:
+ *         level_id: HipAdJgXIAELA4wA3kgz
+ *         collected_points: 1000
+ * 
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: UserLevel
+ *   description: The UserLevel managing API
+ * /api/userLevels:
+ *   post:
+ *     summary: Add a new userLevel
+ *     tags: [UserLevel]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserLevel'
+ *     responses:
+ *       200:
+ *         description: The userLevel has been added.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UserLevel'
+ *       500:
+ *         description: Server error
+ * /api/userLevels/{id}:
+ *   get:
+ *     summary: Get a userLevel
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [UserLevel]
+ *     responses:
+ *       200:
+ *         description: List of all userLevels.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                $ref: '#/components/schemas/UserLevel'
+ *   put:
+ *     summary: Update Specific userLevel by userLevel id
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [UserLevel]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserLevel'
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: UserLevel ID
+ *     responses:
+ *       200:
+ *         description: update userLevel by userLevel id.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                $ref: '#/components/schemas/UserLevel'
+ *   delete:
+ *     summary: Delete Specific userLevel by userLevel id
+ *     security:
+ *       - bearerAuth: []
+ *     tags: [UserLevel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: UserLevel ID
+ *     responses:
+ *       200:
+ *         description: delete userLevel by userLevel id.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                $ref: '#/components/schemas/UserLevel'
+ */
+
 // Define routes for the Level resource
 router.post('/userLevels', addUserLevel);
 router.get('/userLevels/:id', getLevelbyUser);
